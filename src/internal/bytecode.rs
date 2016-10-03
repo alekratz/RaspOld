7e920848473a8d65b2de0952a5f3687586d2917f
Alek Ratzloff <alekratz@gmail.com>
2016-10-03 16:03:44 -0400

    Add "pop" bytecode

--- src/internal/bytecode.rs
+++ src/internal/bytecode.rs
@@ -5,7 +5,7 @@ pub enum Bytecode {
     Nop,
     Call(String),           // Calls a function, using the current working stack for the args
     Push(Atom),             // Pushes a value to the working stack
-    //Pop(Option<String>),  // Pops a value off of the stack, optionally into an identifier (or register?)
+    Pop(Option<String>),    // Pops a value off of the stack, optionally into an identifier (or register?)
     FunDef(Function),       // Defines a function that will be compiled upon its first use
     Label(u64),             // Defines a location that can be jumped to
     Jump(u64),              // Jumps to a label that's been defined
